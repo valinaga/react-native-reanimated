ff5c9922c2f45ed74f8dc31af0d5e288e5fc54c2
Vali Nagacevschi <vali.nagacevschi@electricjukebox.com>
2019-08-20 14:24:06 +0100

    ok

--- android/src/main/java/com/swmansion/reanimated/nodes/AudioNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/AudioNode.java
@@ -4,8 +4,10 @@ package com.swmansion.reanimated.nodes;
 import com.facebook.react.bridge.ReadableMap;
 import com.swmansion.reanimated.NodesManager;
 import java.lang.Math;
+import java.util.Log;
 
 public class AudioNode extends Node implements NodesManager.OnAnimationFrame {
+  private final static String TAG = "REANIMATED NATIVE AUDIONODE";
   public boolean isPlaying = false;
   private int index = 2;
   private static Double[] stopValues = {0.1, 0.2, 0.3};
@@ -32,14 +34,17 @@ public class AudioNode extends Node implements NodesManager.OnAnimationFrame {
   @Override
   protected Double evaluate() {
     if (isPlaying) {
+      Log.d(TAG, "Value ON: " + value);
       return value + 1; // playValues[index];
     }
+    Log.d(TAG, "Value OFF: " + value);
     return value + 2; // stopValues[index];
   }
 
   @Override
   public void onAnimationFrame() {
     value = Math.random();
+    Log.d(TAG, "onAnimationFrame: " + value);
     if (isPlaying) {
       markUpdated();
       mNodesManager.postOnAnimation(this);
