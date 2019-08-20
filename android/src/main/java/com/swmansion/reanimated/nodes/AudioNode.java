6b9b3f793383b43bc8bda0b6d56ee15ce736acd1
Vali Nagacevschi <vali.nagacevschi@electricjukebox.com>
2019-08-20 14:01:52 +0100

    some changes

--- android/src/main/java/com/swmansion/reanimated/nodes/AudioNode.java
+++ android/src/main/java/com/swmansion/reanimated/nodes/AudioNode.java
@@ -10,6 +10,7 @@ public class AudioNode extends Node implements NodesManager.OnAnimationFrame {
   private int index = 2;
   private static Double[] stopValues = {0.1, 0.2, 0.3};
   private static Double[] playValues = {0.5, 0.6, 0.7};
+  private Double value = 0.0;
 
   public AudioNode(int nodeID, ReadableMap config, NodesManager nodesManager) {
     super(nodeID, config, nodesManager);
@@ -31,13 +32,14 @@ public class AudioNode extends Node implements NodesManager.OnAnimationFrame {
   @Override
   protected Double evaluate() {
     if (isPlaying) {
-      return Math.random(); // playValues[index];
+      return value + 1; // playValues[index];
     }
-    return Math.random(); // stopValues[index];
+    return value + 2; // stopValues[index];
   }
 
   @Override
   public void onAnimationFrame() {
+    value = Math.random();
     if (isPlaying) {
       markUpdated();
       mNodesManager.postOnAnimation(this);
